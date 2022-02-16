93f8127a4125688b6c4554e369c27521efaafe60
Yair <32864499+bluepostit@users.noreply.github.com>
2022-02-16 18:07:11 +0100

    finished livecode: no bootstrap

--- README.md
+++ README.md
@@ -1,24 +1,7 @@
-# README
+# Parks and Plants
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+## User Stories
+- As a user I can see one garden
+- As a user I can see the plants of a garden
+- As a user I can add a plant to a garden
+- As a user I can delete a plant
---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,29 @@
+class PlantsController < ApplicationController
+  def create
+    # find the garden
+    # create new plant
+    # associate plant with the garden
+    # save it
+    # redirect to garden show page
+    @garden = Garden.find(params[:garden_id])
+    @plant = Plant.new(plant_params)
+    @plant.garden = @garden
+    if @plant.save
+      redirect_to garden_path(@garden)
+    else
+      render 'gardens/show'
+    end
+  end
+
+  def destroy
+    @plant = Plant.find(params[:id])
+    @plant.destroy
+    redirect_to garden_path(@plant.garden)
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :image_url)
+  end
+end
